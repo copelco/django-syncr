e329549d76b596b55eb64cb8fb980c6d5662cd3b
haineault <haineault@d7e86a57-8a46-0410-862b-39116e8ba8cc>
2009-06-11 03:38:16 +0000

    [subversion] fixed a small bug
    git-svn-id: http://django-syncr.googlecode.com/svn/branches/haineault@90 d7e86a57-8a46-0410-862b-39116e8ba8cc

--- subversion/admin.py
+++ subversion/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from syncr.subversion.models import Revision, SubversionSync
 
 
-class SubversionSyncAdmin(ModelAdmin):
+class SubversionSyncAdmin(admin.ModelAdmin):
     list_display   = ('url', 'username', 'rev_start', 'rev_end', 'limit', 'include_merged_rev', 'discover_changed_paths')
     list_filter    = ('username',)
     search_fields  = ['url', 'username',]
-admin_site.register(SubversionSync, SubversionSyncAdmin)
-
+admin.site.register(SubversionSync, SubversionSyncAdmin)
 
 class RevisionAdmin(admin.ModelAdmin):
     list_display   = ('revision', 'message', 'author', 'date', 'svnPath')
     list_filter    = ('author', 'date', 'svnPath')
     search_fields  = ['message', 'author', 'revision']
     date_hierarchy = 'date'
+    actions = []
 admin.site.register(Revision, RevisionAdmin)
